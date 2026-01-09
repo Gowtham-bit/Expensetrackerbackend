7c7b739147499214e6f8cd6a4b952ab2b00ae52e
Gowtham-bit <gowthamsivakumar001@gamil.com>
2026-01-09 09:21:11 +0530

    Changes

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 mongoose.connect("mongodb+srv://gowthamsivakumar001_db_user:sgm2006@cluster0.nu37wb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-.then(() => console.log("MongoDB Atlas connected"))
-.catch(err => console.log("MongoDB connection error",err));
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("MongoDB Atlas connected"))
+    .catch(err => console.log("MongoDB connection error", err));
 
 const expenseSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  amount: { type: Number, required: true },
+    title: { type: String, required: true },
+    amount: { type: Number, required: true },
 });
 const Expense = mongoose.model("Expense", expenseSchema);
 
@@ -27,8 +28,8 @@ async function updateExpense(req, res) {
     try {
         const { id, title, amount } = req.body;
         console.log(req.body);
-        
-        await Expense.findByIdAndUpdate(id, { title, amount }, {new : true});
+
+        await Expense.findByIdAndUpdate(id, { title, amount }, { new: true });
         res.send("Expense updated");
     } catch (error) {
         res.status(500).send("Error in updating expense");
